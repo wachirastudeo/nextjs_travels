c0809712f9b63d689218fede591c420642cc9c12
wachira <wachirastudeo@gmail.com>
2025-01-10 22:05:19 +0700

    css start favorite

--- src/components/card/LandmarkCard.tsx
+++ src/components/card/LandmarkCard.tsx
@@ -1,19 +1,24 @@
 
 import { LandmarkCardProps } from "@/utils/types"
 import Image from "next/image"
+import LandmarkRating from "./LandmarkRating"
 const LandmarkCard = ({ landmark }: { landmark: LandmarkCardProps }) => {
     const { name, image, id, province, price, lat, lng, category } = landmark
     return (
         <article className="group relative">
-            <div className="relative h-[300px]">
+            <div className="relative h-[300px] rounded-md mb-2">
                 {name}
                 <Image
                     src={image}
                     alt={name}
                     sizes="(max-width: 768px) 100vw ,50vw"
                     fill
-                    className="object-cover"
+                    className="object-cover rounded-md group-hover:scale-105 transition-transform duration-500"
                 />
+            </div>
+            <div className="flex  justify-between items-center">
+                <h3 className="text-sm font-semibold mt-1">{name} </h3>
+                <LandmarkRating />
             </div>
         </article>
     )
---
+++ src/components/card/LandmarkRating.tsx
@@ -0,0 +1,8 @@
+import { Star } from "lucide-react"
+
+const LandmarkRating = () => {
+    return (
+        <span><Star className="h-4 w-4" /></span>
+    )
+}
+export default LandmarkRating
