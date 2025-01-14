7bdd34862f9106585945bd8b33ace724989f5465
wachira <wachirastudeo@gmail.com>
2025-01-14 19:22:15 +0700

    supabase

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,23 +3,21 @@ const bucket='landmark-bucket'
 const url= process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const key= process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 // Create Supabase client
-const supabase = createClient(url, key)
+const supabase = createClient(url, key);
+
 // Upload file using standard upload
 export async function uploadFile(image: File) {
-
-    const timeStamp = Date.now()
-    const newName = `Travel-${timeStamp}-${image.name}`
+  const timeStamp = Date.now();
+  const newName = `Travel-${timeStamp}-${image.name}`;
 
   const { data, error } = await supabase.storage
-  .from(bucket)
-  .upload(newName, image,{
-      cacheControl: '3600',
-      upsert: false,
-  })
-    if(!data) throw new Error("Failed to upload file") //error
- 
-
-    return supabase.storage
     .from(bucket)
-    .getPublicUrl(newName).data.publicUrl
+    .upload(newName, image,{
+      cacheControl:'3600'
+    });
+
+  if (!data) throw new Error("Image upload failed!!!");
+  return supabase.storage
+  .from(bucket)
+  .getPublicUrl(newName).data.publicUrl
 }
